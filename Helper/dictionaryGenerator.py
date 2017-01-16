1573cc11a8d7226e04a26053f42e84f2990cc58b
BrendanPC <brendanpcoughlin@gmail.com>
2017-01-16 18:18:37 -0500

    Added settings options Word Difficulty and Word Density

--- Helper/dictionaryGenerator.py
+++ Helper/dictionaryGenerator.py
@@ -2,6 +2,8 @@ f = open('dictionary.txt', 'r', encoding='utf-8')
 dict = open('dictionary.js', 'w', encoding='utf-8')
 dict.write('var DICTIONARY = [')
 
+f = sorted(f, key=lambda x: len(x.split(',')[1]))
+
 for line in f:
 	words = line.split(',')
 	dict.write('[\"' + words[0].rstrip() + '\",\"' + words[1].rstrip() + '\",\"' + words[2].rstrip() + '\"],\n')
